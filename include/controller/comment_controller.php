5c23b792caf43cbc3ca2be872df36ecd8ebc2ed1
JiangGle <599580031@qq.com>
2017-04-12 22:53:39 +0800

    将“评论发表状态”的提示改为“用json输出对应状态码”

--- include/controller/comment_controller.php
+++ include/controller/comment_controller.php
@@ -32,29 +32,29 @@ class Comment_Controller {
 		$Comment_Model = new Comment_Model();
 		$Comment_Model->setCommentCookie($name,$mail,$url);
 		if($Comment_Model->isLogCanComment($blogId) === false) {
-			emMsg('评论失败：该文章已关闭评论');
+			myJson(json_encode(array("status"=>"1")));
 		} elseif ($Comment_Model->isCommentExist($blogId, $name, $content) === true) {
-			emMsg('评论失败：已存在相同内容评论');
+			myJson(json_encode(array("status"=>"2")));
 		} elseif (ROLE == ROLE_VISITOR && $Comment_Model->isCommentTooFast() === true) {
-			emMsg('评论失败：您提交评论的速度太快了，请稍后再发表评论');
+			myJson(json_encode(array("status"=>"3")));
 		} elseif (empty($name)) {
-			emMsg('评论失败：请填写姓名');
+			myJson(json_encode(array("status"=>"4")));
 		} elseif (strlen($name) > 20) {
-			emMsg('评论失败：姓名不符合规范');
+			myJson(json_encode(array("status"=>"5")));
 		} elseif ($mail != '' && !checkMail($mail)) {
-			emMsg('评论失败：邮件地址不符合规范');
+			myJson(json_encode(array("status"=>"6")));
 		} elseif (ISLOGIN == false && $Comment_Model->isNameAndMailValid($name, $mail) === false) {
-			emMsg('评论失败：禁止使用管理员昵称或邮箱评论');
+			myJson(json_encode(array("status"=>"7")));
 		} elseif (!empty($url) && preg_match("/^(http|https)\:\/\/[^<>'\"]*$/", $url) == false) {
-			emMsg('评论失败：主页地址不符合规范','javascript:history.back(-1);');
+			myJson(json_encode(array("status"=>"8")));
 		} elseif (empty($content)) {
-			emMsg('评论失败：请填写评论内容');
+			myJson(json_encode(array("status"=>"9")));
 		} elseif (strlen($content) > 8000) {
-			emMsg('评论失败：内容不符合规范');
+			myJson(json_encode(array("status"=>"10")));
 		} elseif (ROLE == ROLE_VISITOR && Option::get('comment_needchinese') == 'y' && !preg_match('/[\x{4e00}-\x{9fa5}]/iu', $content)) {
-			emMsg('评论失败：评论内容需包含中文');
+			myJson(json_encode(array("status"=>"11")));
 		} elseif (ISLOGIN == false && Option::get('comment_code') == 'y' && session_start() && $imgcode != $_SESSION['code']) {
-			emMsg('评论失败：验证码错误');
+			myJson(json_encode(array("status"=>"12")));
 		} else {
             $_SESSION['code'] = null;
 			$Comment_Model->addComment($name, $content, $mail, $url, $imgcode, $blogId, $pid);
